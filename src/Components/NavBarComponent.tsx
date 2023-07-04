a4e70ef0922af1ea1b96da912d0efe3e0a2a6ce9
Wilmer <wilmer_will@outlook.com>
2023-07-04 16:49:01 -0400

    tweaked home not showing

--- src/Components/NavBarComponent.tsx
+++ src/Components/NavBarComponent.tsx
@@ -6,7 +6,7 @@ const NavBarComponent: React.FC = () => {
   return (
     <div className='flex justify-center bg-[#FFA41B]'>
         <nav className='flex text-[#FFF6F4] text-4xl font-bacasime '>
-            <Link className='px-4 py-5 hover:text-blue-400' to='/'>Home</Link>
+            <Link className='px-4 py-5 hover:text-blue-400' to='/'>New Entry</Link>
             <Link className='px-4 py-5 hover:text-blue-400' to='/houses'>Houses</Link>
             {/* <a href="#" className='px-4 py-5'>Home</a>
             <a href="#" className='px-4 py-5'>About</a> */}
