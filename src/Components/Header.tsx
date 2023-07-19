1c1b97b6937df726ef7e6efd16e8d115d5b0f099
Wilmer <wilmer_will@outlook.com>
2023-07-18 21:34:12 -0400

    Heading Changes

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,8 +3,9 @@ import '../globalStyles.css'
 
 function Header (): JSX.Element {
   return (
-    <div className='flex justify-center '>
-        <h1 className='text-blue-400 font-bacasime text-6xl'>Paint Tracker</h1>
+    <div className='text-center'>
+        <h1 className='text-blue-400 font-bacasime text-6xl'>Batista Aquino</h1>
+        <h2 className='content-center text-blue-400 font-bacasime text-6xl'>Paint</h2>
     </div>
   )
 }
